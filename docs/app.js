979d0da091c2daa8e6e38de142403e3620eb5bcc
behzad <behzad.khosravifar@gmail.com>
2018-09-03 18:51:03 +0430

    add some features

--- docs/app.js
+++ docs/app.js
@@ -1,66 +1,100 @@
-console.log(1234)
-
 var app = angular.module('website', ['ui.router']);
 
-app.config(['$locationProvider', function AppConfig($locationProvider) {    
+app.config(['$locationProvider', function AppConfig($locationProvider) {
 
     // enable html5Mode for pushstate ('#'-less URLs)
     $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('!');
 
 }]);
-app.controller('mainCtrl', function ($scope, $location, $http) {
+
+app.controller('mainCtrl', function ($scope, $location, $http, $timeout, $interval, mytime) {
 
     $scope.title = "AngularJs v1.6.9 Sample";
     $scope.click = "Click on Me!";
     $scope.clickMe = function () {
-        $scope.click = $location.absUrl(); // $location service == window.location
-        $scope.nullvalue = '---------------------------------------------------------------------------';
+        $scope.link = $location.absUrl(); // $location service == window.location
+    };
+    $scope.handleMouse = function (ev) {
+        $scope.x = ev.clientX;
+        $scope.y = ev.clientY;
     };
 
+    $http.get("https://get.taaghche.ir/v1/book/929")
+        .then(function (response) {  //First function handles success
+            console.log('get response', response);
+            if (response.status == 200 && response.data != null)
+                $scope.coverUri = response.data.book.coverUri;
+            else
+                alert(response.statusText)
+        }, function (response) { //Second function handles error      
+            console.log('get response', response);
+            alert("Something went wrong");
+        });
 
-    $http.get("https://get.taaghche.ir/v1/book/929").then(function (response) {
-        console.log('1',response);
-        $scope.coverUri = response.data.book.coverUri;
-    });
+    headerChanger($scope, $timeout);
 
+    $scope.time = new Date().toLocaleTimeString();
+    $interval(function () {
+        $scope.time = mytime.toLocaleTimeString();
+    }, 1000);
 });
+
+app.service("mytime", function () {
+    this.toLocaleTimeString = function () { return new Date().toLocaleTimeString(); }
+});
+
+function headerChanger(scope, timeout) {
+    timeout(function () {
+        if (scope.title == "AngularJs")
+            scope.title = "Sample";
+        else
+            scope.title = "AngularJs";
+        headerChanger(scope, timeout);
+    }, 500);
+}
 
 app.controller('namesCtrl', function () {
     this.names = [
-        { name: 'Jani', country: 'Norway' },
-        { name: 'Carl', country: 'Sweden' },
-        { name: 'Margareth', country: 'England' },
-        { name: 'Hege', country: 'Norway' },
-        { name: 'Joe', country: 'Denmark' },
-        { name: 'Gustav', country: 'Sweden' },
-        { name: 'Birgit', country: 'Denmark' },
-        { name: 'Mary', country: 'England' },
-        { name: 'Kai', country: 'Norway' }
+        { id: 1, name: 'Jani', country: 'Norway', color: "red" },
+        { id: 2, name: 'Carl', country: 'Sweden', color: "green" },
+        { id: 3, name: 'Margareth', country: 'England', color: "lightgreen" },
+        { id: 4, name: 'Hege', country: 'Norway', color: "gray" },
+        { id: 5, name: 'Joe', country: 'Denmark', color: "black" },
+        { id: 6, name: 'Gustav', country: 'Sweden', color: "blue" },
+        { id: 7, name: 'Birgit', country: 'Denmark', color: "yellow" },
+        { id: 8, name: 'Mary', country: 'England', color: "brown" },
+        { id: 9, name: 'Behzad', country: 'Iran', color: "red" },
+        { id: 10, name: 'Nadia', country: 'England', color: "lightblue" },
+        { id: 11, name: 'Mar', country: 'Germany', color: "pink" },
+        { id: 12, name: 'Farbod', country: 'Island', color: "purple" },
+        { id: 13, name: 'Ali', country: 'Iran', color: "red" },
+        { id: 14, name: 'Json', country: 'Bulgaria', color: "yellow" },
+        { id: 15, name: 'Nason', country: 'Turky', color: "green" },
+        { id: 16, name: 'Mali', country: 'USA', color: "black" },
+        { id: 17, name: 'Jack', country: 'England', color: "black" },
+        { id: 18, name: 'Jack', country: 'England', color: "black" },
+        { id: 19, name: 'Jack', country: 'England', color: "black" },
+        { id: 20, name: 'Jack', country: 'England', color: "black" },
+        { id: 21, name: 'Jack', country: 'England', color: "black" },
+        { id: 22, name: 'Jack', country: 'England', color: "black" },
+        { id: 23, name: 'Jack', country: 'England', color: "black" },
+        { id: 24, name: 'Jack', country: 'England', color: "black" },
+        { id: 25, name: 'Jack', country: 'England', color: "black" },
+        { id: 26, name: 'Jack', country: 'England', color: "black" },
+        { id: 27, name: 'Jack', country: 'England', color: "black" },
+        { id: 28, name: 'Jack', country: 'England', color: "black" },
+        { id: 29, name: 'Jack', country: 'England', color: "black" },
+        { id: 30, name: 'Jack', country: 'England', color: "black" },
+        { id: 31, name: 'Kai', country: 'Norway', color: "white" }
     ];
     this.orderByMe = function (name) {
         this.myOrderBy = name;
     }
 });
 
-function byProperty(prop) {
-    return function (a, b) {
-        if (typeof a[prop] == "number") {
-            return (a[prop] - b[prop]);
-        } else {
-            return ((a[prop] < b[prop]) ? -1 : ((a[prop] > b[prop]) ? 1 : 0));
-        }
-    };
-};
-
-function orderBy() {
-    return function (list, name) {
-        return list.sort(byProperty(name));
-    }
-}
-
 function myFormat() {
-    return function(x) {
+    return function (x) {
         var i, c, txt = "";
         for (i = 0; i < x.length; i++) {
             c = x[i];
@@ -73,8 +107,42 @@ function myFormat() {
     };
 }
 
+function distinct() {
+    return function (list) {
+        var xList = angular.copy(list);
+        for (i = 0; i < xList.length - 1; i++) { // read array
+            for (j = i + 1; j < xList.length; j++) { // read next index of array
+                var iEj = true;
+                for (prop in xList) {  // read object properties
+                    if (prop.toLowerCase() != "id") { // if property is not ID then
+                        if (xList[i][prop] != xList[j][prop]) {
+                            iEj = false;
+                            break;
+                        }
+                    }
+                }
+                if (iEj) {
+                    xList[j--].remove();
+                }
+            }
+        }
+    };
+}
+
+app.service('hexafy', function () {
+    this.calc = function (x) {
+        return x.toString(16);
+    }
+});
+
+app.filter('hexFormat', ['hexafy', function (hexafy) {
+    return function (x) {
+        return hexafy.calc(x);
+    };
+}]);
+app.filter("distinct", distinct);
 app.filter("myFormat", myFormat);
-app.filter("customOrderBy", orderBy);
+
 
 app.directive("testTag", function () {
     return {
